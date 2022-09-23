abe53d6e2a3c91554e7cd7a33a82ca490d3f51e9
Idooru <shere1765@gmail.com>
2022-09-23 14:47:14 +0900

    [추가] 유효성 검사를 하기 위한 조건 추가

--- src/main/java/com/example/myrestfulservice/helloworld/user/User.java
+++ src/main/java/com/example/myrestfulservice/helloworld/user/User.java
@@ -3,12 +3,21 @@ package com.example.myrestfulservice.helloworld.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min = 2, max = 10, message = "Name은 2글자 이상 10글자 이하로 입력해 주세요.")
+    @NotNull
     private String name;
+
+    @Past
+    @NotNull
     private Date joinDate;
 }
