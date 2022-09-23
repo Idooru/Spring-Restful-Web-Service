bb9416d746c5ee9ef7e8e5c568deafd32703b8c0
Idooru <shere1765@gmail.com>
2022-09-23 14:49:46 +0900

    [추가] 사용자 유무 체크, 수정, 삭제 추가

--- src/main/java/com/example/myrestfulservice/helloworld/user/UserController.java
+++ src/main/java/com/example/myrestfulservice/helloworld/user/UserController.java
@@ -1,7 +1,11 @@
 package com.example.myrestfulservice.helloworld.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,18 +18,53 @@ public class UserController {
         this.service = service;
     }
 
-    @GetMapping("/")
+    @GetMapping("")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
-    public User retrieveOneUser(@PathVariable int id) {
-        return service.findOne(id);
+    public User retrieveOneUser(@Valid @PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
-    @PostMapping("/")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public void modifyUserName(@RequestBody User user, @PathVariable int id) {
+        String userName = user.getName();
+        User beforeUser = service.findOne(id);
+
+        if (beforeUser == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        service.modifyUserName(beforeUser, userName);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
     }
 }
